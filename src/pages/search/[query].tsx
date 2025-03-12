d786526283fe46a11b2c97f30d6d6d78df25d468
Rajia-Khan <rajia@pebitech.com>
2025-03-12 10:13:42 +0500

    fix: sidebar

--- src/pages/search/[query].tsx
+++ src/pages/search/[query].tsx
@@ -3,11 +3,11 @@ import React from "react";
 
 const Search = () => {
   return (
-    <div className="flex min-h-screen">
-      <div className="w-[300px] p-8 shadow-md shadow-black/20">
+    <div className="flex">
+      <div className="w-[400px] p-8 shadow-md shadow-black/20 h-screen sticky top-0 self-start  overflow-y-auto">
         <h1 className="text-xl font-semibold">Filter</h1>
         <h2 className="text-md font-semibold mt-5">Category</h2>
-        <div className="mt-5">
+        <div className="mt-5 space-y-2">
           <div>
             <input type="checkbox" /> Category 1
           </div>
@@ -24,10 +24,12 @@ const Search = () => {
             <input type="checkbox" /> Category 1
           </div>
         </div>
+
         <h2 className="text-md font-semibold mt-5">Price Range</h2>
         <input type="range" name="" className="w-full" id="" />
       </div>
-      <div className="w-full flex h-full overflow-y-scroll">
+
+      <div className="flex-grow ">
         <Results />
       </div>
     </div>
