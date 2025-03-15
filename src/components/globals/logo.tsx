1f6ba07240df5dcb72d85e5235fe06d6aecdeffb
MuhammadZan <zain@pebitech.com>
2025-03-15 14:31:33 +0500

    add: home link

--- src/components/globals/logo.tsx
+++ src/components/globals/logo.tsx
@@ -1,17 +1,22 @@
 import { BRILLANT_REGULAR } from "@/app/fonts";
 import logo from "@/app/images/logo.png";
 import Image from "next/image";
+import Link from "next/link";
 const Logo = ({ className }: { className?: string }) => {
   return (
     <div className={`${className}`}>
-      <div className="relative flex items-center w-[250px]">
-        <Image
-          src={logo}
-          className="w-[75px] object-contain"
-          alt="florra haven"
-        />
-        <h1 className={`${BRILLANT_REGULAR.className} text-2xl -ml-8 mt-5`}>Plantly</h1>
-      </div>
+      <Link href={"/"}>
+        <div className="relative flex items-center w-[250px]">
+          <Image
+            src={logo}
+            className="w-[75px] object-contain"
+            alt="florra haven"
+          />
+          <h1 className={`${BRILLANT_REGULAR.className} text-2xl -ml-8 mt-5`}>
+            Plantly
+          </h1>
+        </div>
+      </Link>
     </div>
   );
 };
